7cf75273304b8d45325af1cdb4a1cf6bfd0d3000
XvaLincoln <thapelopoane7@gmail.com>
2025-10-22 10:35:36 +0200

    Complete exercise 1.5: Add Hello child component

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,24 +1,11 @@
-  const Hello = (props) => { 
-    return (
-      <div> 
-        <p> Hello {props.name}, you are {props.age}</p>
-      </div>
-    )
-  } 
-  
+import Hello from "./Hello";
+
 const App = () => {
-  const friends = [
-    { name: 'Thapelo', age: 26 },
-    { name: 'John', age: 30 },
-    { name: 'Jane', age: 20 },
-  ]
   return (
-  <div> 
-   <h1>Greetings</h1>
-   <Hello name = 'Thapelo' age = {26+10} />
-   <Hello name = 'John' age = {30 + 5} />
-   <Hello name = 'Jane' age = {20 + 3}/>
-  </div>
-) 
-} 
-      export default App
+    <div>
+      <Hello name="Thapelo" age={25} />
+    </div>
+  );
+}
+
+export default App;
