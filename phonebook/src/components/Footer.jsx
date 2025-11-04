7adfdde1b729a8a95c8191f63ab6f610e7ecaa3c
XvaLincoln <thapelopoane7@gmail.com>
2025-11-04 16:34:58 +0200

    Add Phonebook and Countries apps

---
+++ phonebook/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => { 
+    const footerStyle = { 
+        color: 'green', 
+        fontStyle: 'italic',
+    } 
+    return ( 
+        <div style={footerStyle}> 
+            PhoneBook app, Fullstack Open 2025!
+            </div>
+    ) 
+} 
+export default Footer
